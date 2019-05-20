63a3dda9daf8443c86510674200e5d8423a06a6a
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-05-20 12:39:30 +0300

    solution1

--- main.js
+++ main.js
@@ -2,10 +2,29 @@ const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const slides = document.querySelector('.container')
 
-next.onclick = function() {
-  slides.scrollLeft += 390;
+
+
+function addDisable() {
+  if (slides.scrollLeft === 0) {
+    prev.classList.add('disabled');
+  }
+
+   else if (slides.scrollLeft === 910) {
+    next.classList.add('disabled');
+  }
+
+   else {
+    prev.classList.remove('disabled');
+    next.classList.remove('disabled');
+  }
 }
 
-prev.onclick = function() {
+ next.onclick = function() {
+  slides.scrollLeft += 390;
+  addDisable();
+}
+
+ prev.onclick = function() {
   slides.scrollLeft -= 390;
+  addDisable();
 }
